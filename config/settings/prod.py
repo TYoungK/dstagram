0e93d8cbcdd2505effcb782b3220f3aeaae29de8
TYoungK <xordud525@nate.com>
2022-10-13 09:10:20 +0900

    fixing...

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -19,3 +19,29 @@ DATABASES = {
         'PORT': env('DATABASE_PORT'),
     }
 }
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': True,
+#     'formatters': {
+#         'verbose': {
+#             'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(process)d %(thread)d %(message)s'
+#         }
+#     },
+#     'handlers': {
+#         'gunicorn': {
+#             'level': 'DEBUG',
+#             'class': 'logging.handlers.RotatingFileHandler',
+#             'formatter': 'verbose',
+#             'filename': '/home/app/web/log/gunicorn.errors',
+#             'maxBytes': 1024 * 1024 * 100,  # 100 mb
+#         }
+#     },
+#     'loggers': {
+#         'gunicorn.errors': {
+#             'level': 'DEBUG',
+#             'handlers': ['gunicorn'],
+#             'propagate': True,
+#         },
+#     }
+# }
